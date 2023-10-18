243da411352b37998bde430d17059cc49e180752
Torres Rodrigo <kaioken1811@gmail.com>
2023-10-18 18:21:17 -0300

    Update primer script sql.sql

--- primer script sql.sql
+++ primer script sql.sql
@@ -1,2 +1,10 @@
-SELECT * FROM gamme_schema.usuarios;
+CREATE TABLE usuarios (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  nombre VARCHAR(50),
+  email VARCHAR(100)
+);
+
 ALTER TABLE usuarios ADD COLUMN edad INT;
+INSERT INTO usuarios (nombre, email,edad)
+VALUES
+    ('Laura López', 'laura@example.com',32);
